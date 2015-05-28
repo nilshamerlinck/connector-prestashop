fe0b61d2e37e0921b1735a7d0c6eb4e00afc69f0
Benoit Guillot <benoit.guillot@akretion.com>
2015-05-28 10:03:41 +0200

    [FIX] adapt to new api

--- prestashoperpconnect/consumer.py
+++ prestashoperpconnect/consumer.py
@@ -49,9 +49,7 @@ def delay_export_all_bindings(session, model_name, record_id, fields=None):
     """
     if session.context.get('connector_no_export'):
         return
-    model = session.pool.get(model_name)
-    record = model.browse(session.cr, session.uid,
-                          record_id, context=session.context)
+    record = session.env[model_name].browse(record_id)
     for binding in record.prestashop_bind_ids:
         export_record.delay(session, binding._model._name, binding.id,
                             fields=fields)
@@ -61,9 +59,7 @@ def delay_unlink(session, model_name, record_id):
     """ Delay a job which delete a record on Prestashop.
 
     Called on binding records."""
-    model = session.pool.get(model_name)
-    record = model.browse(session.cr, session.uid,
-                          record_id, context=session.context)
+    record = session.env[model_name].browse(record_id)
     env = get_environment(session, model_name, record.backend_id.id)
     binder = env.get_connector_unit(Binder)
     external_id = binder.to_backend(record_id)
@@ -76,9 +72,7 @@ def delay_unlink_all_bindings(session, model_name, record_id):
     """ Delay a job which delete a record on Prestashop.
 
     Called on binding records."""
-    model = session.pool.get(model_name)
-    record = model.browse(session.cr, session.uid,
-                          record_id, context=session.context)
+    record = session.env[model_name].browse(record_id)
     for bind_record in record.prestashop_bind_ids:
         prestashop_model_name = bind_record._name
         env = get_environment(session, prestashop_model_name, bind_record.backend_id.id)
