c81c137fb43fde83c7a2936cf1e443444da50077
Alexis de Lattre <alexis.delattre@akretion.com>
2015-10-30 15:50:15 +0100

    Add default value to company_id of delivery.carrier, to avoid blocking the installation of laposte modules
    Port to new API

--- prestashoperpconnect/models/delivery.py
+++ prestashoperpconnect/models/delivery.py
@@ -21,47 +21,31 @@
 ##############################################################################
 
 import logging
-from openerp.osv import fields, orm
+from openerp import models, fields
 
 _logger = logging.getLogger(__name__)
 
 
-class prestashop_delivery_carrier(orm.Model):
+class PrestashopDeliveryCarrier(models.Model):
     _name = 'prestashop.delivery.carrier'
     _inherit = 'prestashop.binding'
     _inherits = {'delivery.carrier': 'openerp_id'}
     _description = 'Prestashop Carrier'
 
-    _columns = {
-        'openerp_id': fields.many2one(
-            'delivery.carrier',
-            string='Delivery carrier',
-            required=True,
-            ondelete='cascade'
-        ),
-        'id_reference': fields.integer(
-            'Id reference',
-            help="In Prestashop, carriers with the same 'id_reference' are "
-                 "some copies from the first one id_reference (only the last "
-                 "one copied is taken account ; and the only one which "
-                 "synchronized with erp)"
-        ),
-        'name_ext': fields.char(
-            'External name',
-            size=64
-        ),
-        'active_ext': fields.boolean(
-            'External active', help="... in prestashop"
-        ),
-        'export_tracking': fields.boolean(
-            'Export tracking numbers',
-            help=" ... in prestashop"
-        ),
-    }
-
-    _defaults = {
-        'export_tracking': False,
-    }
+    openerp_id = fields.Many2one(
+        'delivery.carrier', string='Delivery carrier',
+        required=True, ondelete='cascade')
+    id_reference = fields.Integer(
+        string='Id reference',
+        help="In Prestashop, carriers with the same 'id_reference' are "
+             "some copies from the first one id_reference (only the last "
+             "one copied is taken account ; and the only one which "
+             "synchronized with erp)")
+    name_ext = fields.Char(string='External name', size=64)
+    active_ext = fields.Boolean(
+        string='External active', help="... in prestashop")
+    export_tracking = fields.Boolean(
+        string='Export tracking numbers', help=" ... in prestashop")
 
     _sql_constraints = [
         ('prestashop_uniq', 'unique(backend_id, prestashop_id)',
@@ -69,14 +53,13 @@ class prestashop_delivery_carrier(orm.Model):
     ]
 
 
-class delivery_carrier(orm.Model):
+class DeliveryCarrier(models.Model):
     _inherit = "delivery.carrier"
-    _columns = {
-        'prestashop_bind_ids': fields.one2many(
-            'prestashop.delivery.carrier',
-            'openerp_id',
-            string='PrestaShop Bindings',),
-        'company_id': fields.many2one(
-            'res.company', 'Company', select=1, required=True
-        ),
-    }
+
+    prestashop_bind_ids = fields.One2many(
+        'prestashop.delivery.carrier', 'openerp_id',
+        string='PrestaShop Bindings')
+    company_id = fields.Many2one(
+        'res.company', string='Company', select=1, required=True,
+        default=lambda self: self.env['res.company']._company_default_get(
+            'delivery.carrier'))
