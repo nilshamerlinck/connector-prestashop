975a42e7707f45013fb567cbc0156bdc8226af31
Benoit <benoit.guillot@akretion.com.br>
2018-05-29 15:49:51 +0200

    [ADD] module to support separate categories

---
+++ connector_prestashop_separate_categories/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2011-2013 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Prestashop-Odoo Separate Categories",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "connector_prestashop"
+    ],
+    "author": "Akretion,"
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/connector-prestashop",
+    "category": "Connector",
+    "data": [
+        'product_view.xml',
+    ],
+    'installable': True,
+}
---
+++ connector_prestashop_separate_categories/product.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+from odoo.addons.connector.components.mapper import mapping, changed_by
+from odoo.addons.component.core import Component
+
+
+class PrestashopProductTemplate(models.Model):
+    _inherit = 'prestashop.product.template'
+
+    default_categ_id = fields.Many2one(comodel_name='product.category',
+                                       string='Default category')
+
+
+class ProductTemplateCategoryExportMapper(Component):
+    _name = 'separate.prestashop.product.template.export.mapper'
+    _inherit = 'prestashop.product.template.export.mapper'
+    _apply_on = 'prestashop.product.template'
+
+    @changed_by('default_categ_id')
+    @mapping
+    def default_categ_id(self, record):
+        binder = self.binder_for('prestashop.product.category')
+        ext_id = binder.to_external(record.default_categ_id.id, wrap=True)
+        if ext_id:
+            return {'id_category_default': ext_id}
+        return {}
+
+    def _get_product_category(self, record):
+        """
+        Don't use the main categ but the default categ.
+        """
+        ext_categ_ids = []
+        binder = self.binder_for('prestashop.product.category')
+        categories = list(set(record.categ_ids + record.default_categ_id))
+        for category in categories:
+            ext_id = binder.to_external(category.id, wrap=True)
+            if ext_id:
+                ext_categ_ids.append({'id': ext_id})
+        return ext_categ_ids
